13fd06c3f0c67b9ea7971958232346419491e707
Jyotirmoy <bravo68web@gmail.com>
2020-10-24 12:30:18 +0530

    Initial Code

---
+++ index.js
@@ -0,0 +1,16 @@
+const urls = require('./urls.json')
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const path = new URL(request.url).pathname.substring(1)
+  console.log(request.headers['user-agent'], new Date(), path)
+  if (path in urls)
+    return new Response(null, {
+      status: 308,
+      headers: { location: urls[path] },
+    })
+  else return new Response('404 not found?', { status: 404 })
+}
